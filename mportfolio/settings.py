f86ce23c67bfdaeb458608801010ba089ef34cb3
wshields17 <wshields1711@gmail.com>
2019-03-18 06:42:37 -0500

    318

--- mportfolio/settings.py
+++ mportfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portdb',
         'USER': 'postgres',
-        'PASSWORD':'991550sp',
+        'PASSWORD':'991550Pg',
         'HOST':'localhost',
         'PORT':'5432',
     }
